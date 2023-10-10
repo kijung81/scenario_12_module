cb21ffbfad7c40af87cefd66d3a05fa5fdace079
kijung81 <145954893+kijung81@users.noreply.github.com>
2023-10-10 15:50:04 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,21 +9,21 @@ terraform {
 provider "aws" {
   region = var.region
 }
-module "compute" {
-  source  = "app.terraform.io/hycho-training/compute/aws"
+module "scenario_12_module_compute" {
+  source  = "app.terraform.io/Golfzon/scenario_12_module_compute/aws"
   version = "1.0.0"
   prefix  = var.prefix
   subnet_id = module.network.aws_subnet_id
   security_group = module.security.security_group_id
 }
-module "network" {
-  source  = "app.terraform.io/hycho-training/network/aws"
+module "scenario_12_module_network" {
+  source  = "app.terraform.io/Golfzon/scenario_12_module_network/aws"
   version = "1.0.0"
   prefix  = var.prefix
   region  = var.region
 }
-module "security" {
-  source  = "app.terraform.io/hycho-training/security/aws"
+module "scenario_12_module_security" {
+  source  = "app.terraform.io/Golfzon/scenario_12_module_security/aws"
   version = "1.0.0"
   prefix  = var.prefix
   vpc_id  = module.network.aws_vpc_id
